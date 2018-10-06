a9a5e7e8bc0bd69855adb957d7946f814f20c774
timostrating <timostrating@home.nl>
2018-10-06 13:29:39 +0200

    nearly finished week 4

--- Python/exercices/week4/week4.py
+++ Python/exercices/week4/week4.py
@@ -94,23 +94,32 @@ def sorted_result(books):
 
 pprint(sorted_result(books))
 
-"""
 
 
-# opgave 3
+
+# opgave 3  TODO
 from tkinter import *
 import tkinter.messagebox
 from tkinter.filedialog import askopenfilename
+from pprint import pprint
+import json
 
 def show_result():
     analyze_file(filename.get())
 
 def analyze_file(filename):
     try:
-        infile = open(filename, "r")
+        with open(filename) as f:
+            json_input = json.load(f)
     
         # your code
         counts = {0 : 3, 1 : 7, 2 : 5}
+        i = 3
+        for item in json_input:
+            counts[i] = item["price"]
+            i += 1
+
+        pprint(counts)
 
         # display a histogram for counts
         show_histogram(counts)
@@ -127,7 +136,7 @@ def show_histogram(counts):
     height = int(canvas["height"])
     heightBar = 0.75 * height
     widthBar = width - 20
-    maxCounts = max(counts)
+    maxCounts = max(counts.values())
     
     for i in range(numberOfBars):
         canvas.create_rectangle(i * widthBar / numberOfBars + 10, height - 20 - counts[i] * heightBar / maxCounts, (i + 1) * widthBar / numberOfBars + 10, height - 20)
@@ -151,3 +160,125 @@ Button(frame2, text = "Browse", command = open_file).pack(side = LEFT)
 Button(frame2, text = "Show Result", command = show_result).pack(side = LEFT)
 
 window.mainloop()
+
+
+
+
+# opgave 4 TODO
+
+
+
+
+# opgave 5
+from collections import namedtuple
+
+Node = namedtuple("Node", "data, left, right")
+
+tree = Node(1,
+            Node(2,
+                Node(4,
+                Node(7, None, None),
+                None),
+            Node(5, None, None)),
+        Node(3,
+            Node(6,
+                Node(8, None, None),
+                Node(9, None, None)),
+            None))
+
+def preorder(node):
+    print(node.data)
+    if node.left != None:
+        preorder(node.left)
+    if node.right != None:
+        preorder(node.right)
+
+preorder(tree)
+
+
+
+
+"""
+# opgave 6
+
+def to_board(s):
+    """Input is a string of space-separated rows filled with N^2 numbers.
+       Result is a list of size N^2."""
+    s = s.replace("\n", " ")
+    return [int(x) for x in s.split()]
+
+# only hor & ver, no wrapping
+def neighbors(i):
+    n = ()
+    row = i//N # [0..N-1]
+
+    # your code
+    
+
+    return n
+
+def display(board):
+    "input is a list of integers, print the list as a board"
+    for i in range(0, SIZE, N):
+        s = ''
+        for x in board[i:i+N]: # extract the rows
+            assert x < 100
+            s = s + '{:3d}'.format(x)
+        print(s)
+
+#s = """
+#0  0  0  0  0  0  0  0 81
+#0  0 46 45  0 55 74  0  0
+#0 38  0  0 43  0  0 78  0
+#0 35  0  0  0  0  0 71  0
+#0  0 33  0  0  0 59  0  0
+#0 17  0  0  0  0  0 67  0
+#0 18  0  0 11  0  0 64  0
+#0  0 24 21  0  1  2  0  0
+#0  0  0  0  0  0  0  0  0 """
+
+s = """
+1 0 0
+0 5 0
+0 0 9 """
+
+b = to_board(s)
+print("the board> ", b)
+print()
+
+SIZE = len(b) # the total size, e.g. 81
+N = int(SIZE**0.5) # size of row or column, e.g. 9
+
+# extract and sort the clues
+clues = sorted([int(x) for x in b if x != 0])
+assert clues[0] == 1
+assert clues[-1] == SIZE # last clue must equal SIZE
+
+
+# pos = index in list b, count = distance starting from 1, clue_index = index in list clues
+# note : will try all paths & will find all solutions
+def solve(pos, count, clue_index):
+    # for i in 
+    print(pos, count, clue_index)
+
+
+pos = b.index(1)
+solve(pos, 1, 0)
+
+
+
+
+
+
+
+
+
+# Extra
+def toStr(n,base):
+   convertString = "0123456789ABCDEF"
+   if n < base:
+      return convertString[n]
+   else:
+      return toStr(n//base,base) + convertString[n%base]
+
+# print(toStr(1453,16))
