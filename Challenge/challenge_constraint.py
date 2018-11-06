662ea4274364afe33565c646e530299c2d3f7ffa
timostrating <timostrating@home.nl>
2018-11-06 11:22:49 +0100

    more python code

--- Challenge/challenge_constraint.py
+++ Challenge/challenge_constraint.py
@@ -6,20 +6,29 @@ from ortools.constraint_solver import pywrapcp
 def test():
     solver = pywrapcp.Solver("simple_example")
     # Create the variables.
-    V = solver.IntVar(1, 8, "V")
-    D = solver.IntVar(1, 8, "D")
-    C = solver.IntVar(1, 8, "C")
-    R = solver.IntVar(1, 8, "R")
-    E = solver.IntVar(1, 8, "E")
-    A = solver.IntVar(1, 8, "A")
-    U = solver.IntVar(1, 8, "U")
-    G = solver.IntVar(1, 8, "G")
+    V = solver.IntVar(1, 9, "V")
+    D = solver.IntVar(1, 9, "D")
+    C = solver.IntVar(1, 9, "C")
+    R = solver.IntVar(1, 9, "R")
+    E = solver.IntVar(1, 9, "E")
+    A = solver.IntVar(1, 9, "A")
+    U = solver.IntVar(1, 9, "U")
+    G = solver.IntVar(1, 9, "G")
 
     # VVD
     # Dzz
     # CDA
     #  CU
     # REG
+    
+    solver.Add(V != 6)
+    solver.Add(D != 6)
+    solver.Add(C != 6)
+    solver.Add(R != 6)
+    solver.Add(E != 6)
+    solver.Add(A != 6)
+    solver.Add(U != 6)
+    solver.Add(G != 6)
 
     # Create the constraints.
     solver.Add(V != D)
@@ -72,11 +81,13 @@ def test():
 
 def print_solution(solver, *array):
     count = 0
+    print(solver.Branches)
+    # print(solver.VarEvalValEvalPhase)
 
     while solver.NextSolution():
         count += 1
         # print(array)
-    # print("\nNumber of solutions found:", count)
+    print("\nNumber of solutions found:", count)
 
 if __name__ == '__main__':
     import timeit
